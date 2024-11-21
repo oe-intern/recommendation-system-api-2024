cd6c778a2413fcc56fb0e698492b8fdfa4f481c2
tung <ongtung2212002@gmail.com>
2024-11-21 19:32:06 +0700

    feat: add jobs process data from webhook for product.
    - Added `CreateProduct` job to add a new product.
    - Added `DeleteProduct` job to delete a product.
    - Added `UpdateProduct` job to update product details for a shop.

---
+++ app/Jobs/DeleteProduct.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Contracts\Commands\Product as ProductCommand;
+use App\Contracts\Queries\Product as ProductQuery;
+use App\Contracts\Queries\Shop as ShopQuery;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class DeleteProduct implements ShouldQueue
+{
+    use Queueable, Dispatchable;
+
+
+    /**
+     * @var string
+     */
+    protected string $shop_domain;
+
+    /**
+     * @var array
+     */
+    protected array $product;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param string $shop_domain
+     * @param array $product
+     */
+    public function __construct(string $shop_domain, array $product)
+    {
+        $this->shop_domain = $shop_domain;
+        $this->product = $product;
+    }
+
+
+    /**
+     * Execute the job.
+     *
+     * @param ProductCommand $product_command
+     * @param ShopQuery $shop_query
+     * @param ProductQuery $product_query
+     * @return void
+     */
+    public function handle(ProductCommand $product_command, ShopQuery $shop_query, ProductQuery $product_query): void
+    {
+        $shop = $shop_query->getByDomain($this->shop_domain);
+
+        if (!$shop) {
+            return;
+        }
+
+        $product_command->delete($this->product['id'], $shop);
+    }
+}
