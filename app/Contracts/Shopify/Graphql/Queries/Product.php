d617b7470876f38564a74c47a2b0c70c86f4a74b
tung <ongtung2212002@gmail.com>
2024-11-26 23:11:16 +0700

    feat: extend Product interface with fetchById and fetchByIds methods
    - Added `fetchById` method to fetch a product by its ID.
    - Added `fetchByIds` method to fetch a list of products by their IDs.

--- app/Contracts/Shopify/Graphql/Queries/Product.php
+++ app/Contracts/Shopify/Graphql/Queries/Product.php
@@ -10,4 +10,20 @@ interface Product
      * @return array
      */
     public function fetchAll(): array;
+
+    /**
+     * Fetch a product by its ID.
+     *
+     * @param string $id
+     * @return array
+     */
+    public function fetchById(string $id): array;
+
+    /**
+     * Fetch a list of products by their IDs.
+     *
+     * @param array $ids
+     * @return array
+     */
+    public function fetchByIds(array $ids): array;
 }
