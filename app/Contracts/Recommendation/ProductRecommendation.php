4e79fa05a17d5e3b6ce71abd2120c8833a222069
tung <ongtung2212002@gmail.com>
2024-11-27 00:05:33 +0700

    feat: implement product recommendation service for handling product recommendations
    - Added `ProductRecommendationService` implementing `ProductRecommendation` interface.
    - Implemented methods for fetching recommended products based on different recommendation types.
    - Added logic for setting recommended products and recommendation types for a product.
    - Integrated fetching of recommended products through system-based logic.
    - Implemented functionality to get full product information including recommendations.

---
+++ app/Contracts/Recommendation/ProductRecommendation.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Contracts\Recommendation;
+
+use App\Collections\Product as ProductCollection;
+use App\Exceptions\ProductNotFoundException;
+use App\Objects\Enums\RecommendationType;
+
+interface ProductRecommendation
+{
+    /**
+     * Get list of recommended products for a product.
+     *
+     * @param string $shop_domain
+     * @param string $product_id
+     * @return array
+     *
+     * @throws ProductNotFoundException
+     */
+    public function getRecommendedProducts(string $shop_domain, string $product_id): array;
+
+    /**
+     * Set list of recommended products for a product.
+     *
+     * @param string $shop_domain
+     * @param string $product_id
+     * @param array $recommended_products
+     * @param RecommendationType|null $recommendation_type
+     * @return ProductCollection
+     */
+    public function setRecommendedProducts(
+        string $shop_domain,
+        string $product_id,
+        array $recommended_products,
+        ?RecommendationType $recommendation_type
+    ): ProductCollection;
+
+    /**
+     * Set the recommendation type for a product.
+     *
+     * @param string $product_id
+     * @param RecommendationType $recommendation_type
+     * @return ProductCollection
+     */
+    public function setRecommendationType(
+        string $product_id,
+        RecommendationType $recommendation_type
+    ): ProductCollection;
+
+    /**
+     * Get full information of a product (including recommendations).
+     *
+     * @param string $product_id
+     * @return array
+     */
+    public function getFullInfo(string $product_id): array;
+}
