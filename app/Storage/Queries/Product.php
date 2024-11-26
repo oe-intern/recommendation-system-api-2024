6cf763842b85cacb9db351f13a6d8232d92b04ec
tung <ongtung2212002@gmail.com>
2024-11-26 23:44:10 +0700

    refactor: refactor `Product` queries to use `shop_domain` and improve exception handling
    - Updated methods in `Product` query class to use `shop_domain` instead of `ShopCollection`.
    - Replaced method `getByIdsAndShopCollection` with `getByShopDomainAndIds` for better readability and consistency.
    - Added `checkProductExist` method to ensure proper product existence check.
    - Refined methods for retrieval referenced and optional products by simplifying attributes and leveraging direct `ProductCollection` calls.

--- app/Storage/Queries/Product.php
+++ app/Storage/Queries/Product.php
@@ -3,23 +3,55 @@
 namespace App\Storage\Queries;
 
 use App\Collections\Product as ProductCollection;
-use App\Collections\Shop as ShopCollection;
 use App\Contracts\Queries\Product as ProductCommand;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Product implements ProductCommand
 {
     /**
+     * Get a product of a shop by ID.
+     *
+     * @param string $product_id
+     * @return ProductCollection|null
+     */
+    public function getById(string $product_id): ?ProductCollection
+    {
+        return ProductCollection::query()->find($product_id);
+    }
+
+    /**
      * Get list products of a shop by IDs
      *
      * @param array $product_ids
-     * @param ShopCollection $shop
+     * @param string $shop_domain
      * @return array
      */
-    public function getByIdsAndShopCollection(array $product_ids, ShopCollection $shop): array
+    public function getByShopDomainAndIds(string $shop_domain, array $product_ids): array
     {
-        return $shop->products()->filter(function ($product) use ($product_ids) {
-            return in_array($product->id, $product_ids);
-        })->all();
+        $products = ProductCollection::query()
+            ->whereIn('id', $product_ids)
+            ->where('shop_domain', $shop_domain)
+            ->get()
+            ->all();
+
+        return collect($products)->pluck('id')->toArray();
+    }
+
+    /**
+     * Check if the product exist in the database.
+     *
+     * @param string $shop_domain
+     * @param string $product_id
+     * @return ProductCollection
+     *
+     * @throws ModelNotFoundException
+     */
+    public function checkProductExist(string $shop_domain, string $product_id): ProductCollection
+    {
+        return ProductCollection::query()
+            ->where('id', $product_id)
+            ->where('shop_domain', $shop_domain)
+            ->firstOrFail();
     }
 
     /**
@@ -29,65 +61,48 @@ class Product implements ProductCommand
      * @param string $product_id
      * @return ProductCollection|null
      */
-    public function getByShopDomainAndProductId(string $shop_domain, string $product_id): ?ProductCollection
+    public function getByShopDomainAndId(string $shop_domain, string $product_id): ?ProductCollection
     {
-        $shop = ShopCollection::where('domain', $shop_domain)->first();
-        if (!$shop) {
-            return null;
-        }
-        return $shop->products()->where('id', $product_id)->first();
+        return ProductCollection::query()
+            ->where('id', $product_id)
+            ->where('shop_domain', $shop_domain)
+            ->first();
     }
 
     /**
      * Get list of referenced products for recommendation.
      *
-     * @param ShopCollection $shop
      * @param string $product_id
      * @return array
      */
-    public function getReferencedProducts(ShopCollection $shop, string $product_id): array
+    public function getReferencedProducts(string $product_id): array
     {
-        return $this->getProductRecommendationIds($shop, $product_id, 'referencedIds');
-    }
-
-    /**
-     * Get list of product IDs using a specific attribute for recommendation.
-     *
-     * @param ShopCollection $shop
-     * @param string $product_id
-     * @param string $attribute
-     * @return array
-     */
-    private function getProductRecommendationIds(ShopCollection $shop, string $product_id, string $attribute): array
-    {
-        $product = $this->getByIdAndShopCollection($product_id, $shop);
-        if (!$product) {
-            return [];
-        }
-        return $product->getAttributeValue($attribute);
-    }
-
-    /**
-     * Get a product of a shop by ID.
-     *
-     * @param ShopCollection $shop
-     * @param string $product_id
-     * @return ProductCollection|null
-     */
-    public function getByIdAndShopCollection(string $product_id, ShopCollection $shop): ?ProductCollection
-    {
-        return $shop->products()->where('id', $product_id)->first();
+        return $this->getProductRecommendationIds($product_id, 'referencedIds');
     }
 
     /**
      * Get list of optional products for recommendation.
      *
-     * @param ShopCollection $shop
      * @param string $product_id
      * @return array
      */
-    public function getOptionalProducts(ShopCollection $shop, string $product_id): array
+    public function getOptionalProducts(string $product_id): array
     {
-        return $this->getProductRecommendationIds($shop, $product_id, 'optionIds');
+        return $this->getProductRecommendationIds($product_id, 'optionIds');
+    }
+
+    /**
+     * Get list of product IDs using a specific attribute for recommendation.
+     *
+     * @param string $product_id
+     * @param string $attribute
+     * @return array
+     */
+    private function getProductRecommendationIds(string $product_id, string $attribute): array
+    {
+        return ProductCollection::query()
+            ->where('id', $product_id)
+            ->first()
+            ->getAttributeValue($attribute);
     }
 }
