0b6df7050274ac306e9801f4ff514011c94caf24
tung <ongtung2212002@gmail.com>
2024-11-28 17:55:03 +0700

    feat: add product interaction controller for tracking and filtering interactions
    - Implemented `ProductInteractionController` to handle interactions for products.
    - Added `filter` method to retrieve product interactions based on product ID and date range.
    - Added `interaction` method to increment interaction counts for a specific product.

---
+++ app/Http/Controllers/ProductInteractionController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contracts\Recommendation\IProductInteraction;
+use App\Exceptions\ProductNotFoundException;
+use App\Lib\Utils;
+use App\Services\Shopify\UserContext;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductInteractionController extends Controller
+{
+    /**
+     * @var UserContext
+     */
+    protected UserContext $user_context;
+
+    /**
+     * @var IProductInteraction
+     */
+    protected IProductInteraction $product_interaction_service;
+
+    /**
+     * ProductInteractionController constructor.
+     *
+     * @param UserContext $user_context
+     * @param IProductInteraction $product_interaction_service
+     */
+    public function __construct(UserContext $user_context, IProductInteraction $product_interaction_service)
+    {
+        $this->user_context = $user_context;
+        $this->product_interaction_service = $product_interaction_service;
+    }
+
+    /**
+     * Get list of interactions for a shop.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @throws ProductNotFoundException
+     */
+    public function filter(Request $request): JsonResponse
+    {
+        $shop_domain = $this->user_context->getDomain()->toNative();
+        $product_id = $request->query('product_id');
+        $product_id = $product_id ? Utils::getIdFromGid($product_id) : null;
+        $start_date = $request->query('start_date');
+        $end_date = $request->query('end_date');
+
+        $interactions = $this->product_interaction_service
+            ->filter($shop_domain, $product_id, $start_date, $end_date);
+
+        return response()->json($interactions);
+    }
+
+    /**
+     * Increment the number of interactions for a product.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     *
+     * @throws ProductNotFoundException
+     */
+    public function interaction(Request $request): JsonResponse
+    {
+        $shop_domain = $this->user_context->getDomain()->toNative();
+        $product_id = Utils::getIdFromGid($request->input('product_id'));
+        $number_of_interactions = $request->input('number_of_interactions');
+        $interaction_type = $request->input('interaction_type');
+
+        $this->product_interaction_service->increment(
+            $shop_domain,
+            $product_id,
+            $number_of_interactions,
+            $interaction_type
+        );
+
+        return response()->json(['message' => 'Interactions updated successfully']);
+    }
+}
