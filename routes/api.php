a442a179ca5f58b669f730f40462e5fe7e9b2cad
tung <ongtung2212002@gmail.com>
2024-11-29 15:03:25 +0700

    feat: add routes for auto-recommendation settings
    - Added new routes under `recommendation/settings` and `sdk/recommendation/settings` for auto-recommendation.
    - The `getAutoRecommendation` and `setAutoRecommendation` methods in `RecommendationController` handle these routes.
    - Applied the `validate.auto.recommendation.request` middleware to ensure proper validation.

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
-use App\Http\Controllers\RecommendationController;
 use App\Http\Controllers\ProductInteractionController;
+use App\Http\Controllers\RecommendationController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['access_control_headers', 'shopify.auth', 'verify.token']], function () {
     Route::prefix('recommendation')->group(function () {
         Route::get('/', [RecommendationController::class, 'getProduct']);
+        Route::prefix('settings')->group(function () {
+            Route::get('', [RecommendationController::class, 'getAutoRecommendation']);
+            Route::put('', [RecommendationController::class, 'setAutoRecommendation'])
+                ->middleware('validate.auto.recommendation.request');
+        });
         Route::put('/state', [
             RecommendationController::class,
             'setState'
@@ -25,6 +30,7 @@ Route::group(['middleware' => ['access_control_headers', 'shopify.auth', 'verify
 Route::group(['middleware' => ['access_control_headers', 'identify.shop.domain']], function () {
     Route::prefix("sdk")->group(function () {
         Route::get("/recommendation", [RecommendationController::class, 'getRecommendation']);
+        Route::get("/recommendation/settings", [RecommendationController::class, 'getAutoRecommendation']);
         Route::prefix("/interaction")->group(function () {
             Route::post('', [ProductInteractionController::class, 'interaction'])
                 ->middleware('validate.product.interaction.request');
