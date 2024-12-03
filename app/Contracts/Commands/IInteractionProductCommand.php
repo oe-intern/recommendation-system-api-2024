c8b440803ead7b4b10e0e21a4341b8c256c36d69
tung <ongtung2212002@gmail.com>
2024-12-03 22:02:43 +0700

    Update: command interface uses shop_id instead of shop_domain and modify recommendation methods

--- app/Contracts/Commands/IInteractionProductCommand.php
+++ app/Contracts/Commands/IInteractionProductCommand.php
@@ -9,36 +9,35 @@ interface IInteractionProductCommand
     /**
      * Increment any interaction of a product from a shop.
      *
-     * @param string $shop_domain
+     * @param string $shop_id
      * @param string $product_id
      * @param InteractionType $interaction_type
-     * @param int $quantity
+     * @param int|null $quantity
      * @return void
      */
     public function increment(
-        string $shop_domain,
+        string $shop_id,
         string $product_id,
         InteractionType $interaction_type,
-        int $quantity
+        ?int $quantity
     ): void;
 
     /**
      * Increment the clicks of a product from a shop.
      *
-     * @param string $shop_domain
+     * @param string $shop_id
      * @param string $product_id
-     * @param int $quantity
      * @return void
      */
-    public function incrementClicks(string $shop_domain, string $product_id, int $quantity = 1): void;
+    public function incrementClicks(string $shop_id, string $product_id): void;
 
     /**
      * Increment the views of a product from a shop.
      *
-     * @param string $shop_domain
+     * @param string $shop_id
      * @param string $product_id
-     * @param int $quantity
+     * @param int|null $quantity
      * @return void
      */
-    public function incrementAddToCart(string $shop_domain, string $product_id, int $quantity = 1): void;
+    public function incrementAddToCart(string $shop_id, string $product_id, ?int $quantity = 1): void;
 }
