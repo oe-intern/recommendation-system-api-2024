49ba9f35230cb4f27f27e3ec50a661a359372498
tung <ongtung2212002@gmail.com>
2024-12-09 18:16:44 +0700

    Update: rename middleware for event & update validator

--- app/Http/Middleware/EventAnalyticRequest.php
+++ app/Http/Middleware/EventAnalyticRequest.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Middleware;
 
-use App\Objects\Enums\StatisticsGroupBy;
+use App\Objects\Enums\AnalyticGroupBy;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rules\Enum;
 
-class ProductStatistic
+class EventAnalyticRequest
 {
     /**
      * Handle an incoming request.
@@ -21,7 +21,7 @@ class ProductStatistic
         $request->validate([
             'group_by' => [
                 'sometimes',
-                new Enum(StatisticsGroupBy::class),
+                new Enum(AnalyticGroupBy::class),
             ],
             'product_id' => 'sometimes|nullable|string',
             'start_date' => 'required|date_format:Y-m-d|before_or_equal:end_date',
