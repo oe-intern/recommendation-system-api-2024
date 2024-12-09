eb208821447626eb749d54589ccef94b25914658
tung <ongtung2212002@gmail.com>
2024-12-09 20:29:07 +0700

    Update: replace interaction interfaces with event interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,8 @@ use App\Models\Export;
 use App\Policies\ActivityPolicy;
 use App\Contracts\Recommendation\IProductRecommendation;
 use App\Services\Product\ProductRecommendationService;
-use App\Contracts\Recommendation\IProductInteraction;
-use App\Services\Product\ProductInteractionService;
+use App\Contracts\Recommendation\IProductEvent;
+use App\Services\Product\ProductEventService;
 use App\Services\Shopify\UserContext;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\URL;
@@ -56,8 +56,8 @@ class AppServiceProvider extends ServiceProvider
         );
 
         $this->app->bind(
-            IProductInteraction::class,
-            ProductInteractionService::class
+            IProductEvent::class,
+            ProductEventService::class
         );
     }
 
