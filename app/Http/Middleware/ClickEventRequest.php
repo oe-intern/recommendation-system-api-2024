ab43e47f9c7fde6b43fdbd48467611dc5c77492b
tung <ongtung2212002@gmail.com>
2024-12-09 18:12:51 +0700

    Add: `ClickEventRequest` middleware rules for click event request

---
+++ app/Http/Middleware/ClickEventRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ClickEventRequest
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        $request->validate([
+            'product_id' => 'required|string',
+            'data' => 'sometimes|nullable',
+        ]);
+
+        return $next($request);
+    }
+}
