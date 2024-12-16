5b798a18e46d06fb1b2e85132060747a964f7f64
tung2212002 <ongtung2212002@gmail.com>
2024-12-16 23:49:28 +0700

    Add: methods to IRecommendationApi for product recommendation and task state check

--- app/Contracts/ModelRecommendation/IRecommendationApi.php
+++ app/Contracts/ModelRecommendation/IRecommendationApi.php
@@ -25,4 +25,24 @@ interface IRecommendationApi
      * @throws Exception
      */
     public function preRecommend(array $data): array;
+
+    /**
+     * Call the external recommend for 1 product endpoint.
+     *
+     * @param array $data
+     * @return array
+     *
+     * @throws Exception
+     */
+    public function recommendProduct(array $data): array;
+
+    /**
+     * Call the external recommend for check state of task recommendation endpoint.
+     *
+     * @param array $data
+     * @return array
+     *
+     * @throws Exception
+     */
+    public function checkStateTaskRecommendation(array $data): array;
 }
