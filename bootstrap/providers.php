4b8c9eed2fa7347d986a0ca377c46d72daa17c4f
tung <ongtung2212002@gmail.com>
2024-12-26 20:51:48 +0700

    Add: configure Horizon with custom supervisors and service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,7 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\CommandServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\QueryServiceProvider::class,
     App\Providers\ShopifyServiceProvider::class,
     App\Providers\TransformProvider::class,
